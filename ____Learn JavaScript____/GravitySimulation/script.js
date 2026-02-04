9f5e7a1602ff225f8c320e052627c4de6c894281
Otito Amuga <urphasin@gmail.com>
2026-02-03 19:51:29 -0700

    cold.exe()

--- ____Learn JavaScript____/GravitySimulation/script.js
+++ ____Learn JavaScript____/GravitySimulation/script.js
@@ -1,17 +1,17 @@
-let y = 50;
-let v = 0;
-
-const g = 4.0;
-
-function update() {
-    v = v + g;
-    y = y + v;
-
-    if (y > 300) { // ground
-        y = 300;
-        v  =  v * -0.7; // bounce
-    }
-}
+// let y = 50;
+// let v = 0;
+//
+// const g = 4.0;
+//
+// function update() {
+//     v = v + g;
+//     y = y + v;
+//
+//     if (y > 300) { // ground
+//         y = 300;
+//         v  =  v * -0.7; // bounce
+//     }
+// }
 
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
@@ -36,3 +36,20 @@ function update() {
         velocityY = velocityY * (-1*bounce);
     }
 }
+
+function draw() {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    c.beginPath();
+    c.arc(x, y, radius, 0, Math.PI * 2);
+    c.fillStyle = "cyan";
+    c.fill();
+}
+
+function loop() {
+    update();
+    draw();
+    requestAnimationFrame(loop);
+}
+
+loop();
