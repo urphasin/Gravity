18af923331531e8a7ae86c0fc8b4997e46867a29
Otito Amuga <urphasin@gmail.com>
2026-02-08 12:50:07 -0700

    willful

---
+++ GravitySimulation/script.js
@@ -0,0 +1,63 @@
+// let y = 50;
+// let v = 0;
+//
+// const g = 4.0;
+//
+// function update() {
+//     v = v + g;
+//     y = y + v;
+//
+//     if (y > 300) { // ground
+//         y = 300;
+//         v  =  v * -0.7; // bounce
+//     }
+// }
+
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+c.font = "30px Arial";
+c.fillStyle = "blue";
+// Ball properties
+let x = 50;
+let y = 50;
+let size = 30
+
+let v = 0;
+const g = 9.8;
+const bounce = 0.7; // energy loss
+
+
+function draw() {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    c.strokeStyle = "red";
+    c.lineWidth = 4;
+    c.strokeRect(x, y, size, size);
+    
+    
+    c.fillText(("(" + x.toString() + " ," + y.toString() + ")"), 50, 50, 500);
+}
+
+
+let lastTime = performance.now();
+function loop() {
+
+    let now = performance.now();
+    let dt = (now - lastTime) / 500; // convert ms -> seconds
+    lastTime = now;
+
+    if(y >= 120) {
+        v *= -0.8 
+        
+    }
+
+    v += g * dt;
+    y += v * dt;
+
+
+    lastTime = performance.now();
+    draw();
+    requestAnimationFrame(loop);
+}
+
+loop();
